fdb9444e5ed1ed362b7384f0259b67ea3f2eb19b
Mario Guzman Silverio <9472999+MarioGzSl@users.noreply.github.com>
2023-09-19 04:34:17 +0000

    Add feature flags

--- mariogzsl-page/src/app/page.tsx
+++ mariogzsl-page/src/app/page.tsx
@@ -1,21 +1,33 @@
-import { Footer } from "./components/Footer/Footer"
-import { Header } from "./components/Header/Header"
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import dotenv from 'dotenv';
+
+dotenv.config();
+const underConstruction = process.env.UNDER_CONTRUCTION;
 
 export default function Home() {
-
-  const onAbout = () => {
-    console.log('About')
+  if (underConstruction === 'true') {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-6 h-screen">
+        <div className="flex flex-col items-center justify-center w-5/12 h-full">
+          <div className="flex flex-col items-center justify-center w-full h-full">
+            Under construction... It will be ready soon! I promise!
+          </div>
+          <Footer />
+        </div>
+      </main>
+    );
   }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-6 h-screen">
       <div className="flex flex-col items-center justify-center w-5/12 h-full">
-        <Header/>
-        <div className="flex flex-col items-center justify-center w-full h-full">
-          Under construction... It will be ready soon! I promise!!
-        </div>
-        <Footer/>
+        <Header />
+          <div className="flex flex-col items-center justify-center w-full h-full">
+            {/* My code would be here. If I had any. :'(  */}
+          </div>
+        <Footer />
       </div>
     </main>
-  )
+  );
 }
