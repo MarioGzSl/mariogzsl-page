1f3e3757c4af7cf2600d23c69c188a04e79c9d0d
Mario Guzman Silverio <9472999+MarioGzSl@users.noreply.github.com>
2023-09-20 01:03:29 +0000

    Fix code

--- mariogzsl-page/src/app/page.tsx
+++ mariogzsl-page/src/app/page.tsx
@@ -11,6 +11,7 @@ export default function Home() {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-6 h-screen">
         <div className="flex flex-col items-center justify-center w-5/12 h-full">
+          <Header />
           <div className="flex flex-col items-center justify-center w-full h-full">
             Under construction... It will be ready soon! I promise!
           </div>
