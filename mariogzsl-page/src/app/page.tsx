f2311581ab08c04cddf825c28e55bbf86b85b391
Mario Guzman Silverio <9472999+MarioGzSl@users.noreply.github.com>
2023-12-08 07:31:11 +0000

    Fix UI

--- mariogzsl-page/src/app/page.tsx
+++ mariogzsl-page/src/app/page.tsx
@@ -1,18 +1,16 @@
 import { About } from "./components/About/About";
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
-import dotenv from 'dotenv';
 
 import { Analytics } from '@vercel/analytics/react';
 
-dotenv.config();
-const underConstruction = process.env.UNDER_CONTRUCTION;
+const underConstruction = true;
 
 export default function Home() {
-  if (underConstruction === 'true') {
+  if (underConstruction) {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-6 h-screen">
-        <div className="flex flex-col items-center justify-center w-5/12 h-full">
+        <div className="flex flex-col items-center justify-center w-5/12 min-w-[450px] h-full">
           <Header />
           <div className="flex flex-col items-center justify-center w-full h-full">
             Under construction... It will be ready soon! I promise!
