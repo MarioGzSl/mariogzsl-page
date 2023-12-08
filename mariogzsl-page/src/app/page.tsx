6d754bcf7d8ec06b94c064ad1534fad03809f98b
Mario Guzman Silverio <9472999+MarioGzSl@users.noreply.github.com>
2023-12-08 07:42:32 +0000

    Fix UI

--- mariogzsl-page/src/app/page.tsx
+++ mariogzsl-page/src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   if (underConstruction) {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-6 h-screen">
-        <div className="flex flex-col items-center justify-center w-5/12 min-w-[450px] h-full">
+        <div className="flex flex-col items-center justify-center w-5/12 min-w-[320px] h-full">
           <Header />
           <div className="flex flex-col items-center justify-center w-full h-full">
             Under construction... It will be ready soon! I promise!
